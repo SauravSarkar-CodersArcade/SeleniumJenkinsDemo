0398b111ff41b5e14a8e7a8ce6e2a7a99ca6a4df
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2022-08-05 16:43:43 +0530

    First Commit

---
+++ src/test/java/demo/Test1.java
@@ -0,0 +1,39 @@
+package demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	private WebDriver driver;
+
+	@BeforeClass(alwaysRun = true)
+	public void setUp() throws Exception {
+
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", projectPath+"/driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void test1() throws InterruptedException {
+		driver.get("http://automationpractice.com/index.php");
+		Thread.sleep(5000);
+		driver.manage().window().maximize();
+		Thread.sleep(3000);		
+		
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void tearDown() {
+		driver.quit();
+		
+	}
+
+
+}
